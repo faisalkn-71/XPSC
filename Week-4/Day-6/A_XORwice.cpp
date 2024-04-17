41e214078b0e910c935f7a83dd708cae62739cff
faisalkn-71 <csust2021@gmail.com>
2024-04-17 08:33:19 +0800

    2-done

---
+++ Week-4/Day-6/A_XORwice.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long 
+#define endl "\n"
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin>>t;
+    while(t--){
+        int n, m;
+        cin>>n>>m;
+
+        int x = n^m;
+        cout<<x<<endl;
+
+    }
+
+    return 0;
+}
